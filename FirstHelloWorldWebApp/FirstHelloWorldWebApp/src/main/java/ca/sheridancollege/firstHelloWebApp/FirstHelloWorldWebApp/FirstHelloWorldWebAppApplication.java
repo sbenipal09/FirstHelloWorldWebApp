1a6c7d40b2c73c50fdbba0454d46ae8e3331d752
Simranjeet Singh <sin12743@sheridancollege.ca>
2023-09-17 10:14:23 -0400

    done

---
+++ FirstHelloWorldWebApp/FirstHelloWorldWebApp/src/main/java/ca/sheridancollege/firstHelloWebApp/FirstHelloWorldWebApp/FirstHelloWorldWebAppApplication.java
@@ -0,0 +1,13 @@
+	package ca.sheridancollege.firstHelloWebApp.FirstHelloWorldWebApp;
+
+	import org.springframework.boot.SpringApplication;
+	import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+	@SpringBootApplication
+	public class FirstHelloWorldWebAppApplication {
+
+		public static void main(String[] args) {
+			SpringApplication.run(FirstHelloWorldWebAppApplication.class, args);
+		}
+
+	}
