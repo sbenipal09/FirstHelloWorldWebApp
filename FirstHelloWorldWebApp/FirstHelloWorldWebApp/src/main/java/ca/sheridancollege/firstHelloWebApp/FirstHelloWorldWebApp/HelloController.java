5e0a31e5544550f15f332b8d4d44bca75d027424
Simranjeet Singh <sin12743@sheridancollege.ca>
2023-09-17 10:18:51 -0400

    done

--- FirstHelloWorldWebApp/FirstHelloWorldWebApp/src/main/java/ca/sheridancollege/firstHelloWebApp/FirstHelloWorldWebApp/HelloController.java
+++ FirstHelloWorldWebApp/FirstHelloWorldWebApp/src/main/java/ca/sheridancollege/firstHelloWebApp/FirstHelloWorldWebApp/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-
+//controller
     @GetMapping("/index")
     public String showRiteshyadavPage(Model model) {
         return "index";
---
+++ FirstHelloWorldWebApp/FirstHelloWorldWebApp/src/test/java/ca/sheridancollege/firstHelloWebApp/FirstHelloWorldWebApp/FirstHelloWorldWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.firstHelloWebApp.FirstHelloWorldWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstHelloWorldWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
