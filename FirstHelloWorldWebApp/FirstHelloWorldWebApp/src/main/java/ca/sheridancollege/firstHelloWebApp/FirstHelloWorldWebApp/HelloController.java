a3326473c197a350aa88c03e6b441e393475d24b
Simranjeet Singh <sin12743@sheridancollege.ca>
2023-09-17 10:21:54 -0400

    done

--- FirstHelloWorldWebApp/FirstHelloWorldWebApp/src/main/java/ca/sheridancollege/firstHelloWebApp/FirstHelloWorldWebApp/HelloController.java
+++ FirstHelloWorldWebApp/FirstHelloWorldWebApp/src/main/java/ca/sheridancollege/firstHelloWebApp/FirstHelloWorldWebApp/HelloController.java
@@ -3,7 +3,7 @@ package ca.sheridancollege.firstHelloWebApp.FirstHelloWorldWebApp;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+//Controller
 @Controller
 public class HelloController {
 //controller
